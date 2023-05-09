8e76459f0c90e3fa74c9f0199d074704a3c4bd6d
Nadtakan Futhoem <nadtakan.futhoem@tylertech.com>
2023-05-09 13:01:37 -0700

    added cicd config

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,7 @@
-module.exports = {
-  'projectId': '4b7344',
-  e2e: {},
-}
+const { defineConfig } = require('cypress');
+module.exports = defineConfig({
+  'projectId': 'h36rx7',
+  e2e: {
+    setupNodeEvents(on, config) {}
+  },
+});
